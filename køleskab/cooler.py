367b6a623ec59cd8d72d0d9c186a29b9cafe1710
Boltz333 <113584616+Boltz333@users.noreply.github.com>
2024-11-26 10:22:18 +0100

    Skibidi termostat virker

--- køleskab/cooler.py
+++ køleskab/cooler.py
@@ -1,59 +1,74 @@
 import random
 import csv
 
-# Open csv file
-with open('elpris.csv', newline='', encoding='utf-8') as csvfile:
-    csvreader = csv.DictReader(csvfile)
-    
-    # Reading the header
-    header = next(csvreader)  # Skipping to next header
-
-    # Reading and processing each row
-    for row in csvreader:
-        Tid = row['Tid']  # Time is in the first column
-        Pris = row['Pris']  # Price is in the second column
-        print(f"Time: {Tid}, Price: {Pris}")
-
 
 
 
 
 class cooler:
-    def __init__ (self, time, PTime):
-        T = []
-        TKOMP = -5
-        TRUM = 20
-        deltatid = 300
+    def __init__ (self):
+
+        print("check")
+
+        self.TKOMP = -5
+        self.TRUM = 20
+        self.deltatid = 300
+        self.T = []
         TMÅL = 5
-    
-        T[time] = PTime + (self.C1() * (TRUM - PTime) + self.C2() * (TKOMP - PTime)) * deltatid
-        PTime = T(time)
 
-    def C1():
-        if doorclosed:
-            return(5 * 10**-7 * 300**-1)
+
+
+    def coolerupdate(self, time):
+        if time == 0:
+            self.T.append(5)
+            
         else:
-            return(3 * 10**-5 * 300**-1)
+            self.T.append(self.T[time-1] + (self.C1() * (self.TRUM - self.T[time-1]) + self.C2(self.T[time-1]) * (self.TKOMP - self.T[time-1])) * self.deltatid)
+
+
+
+    def C1(self):
+        doorChance = random.randrange(0,9)
+        if doorChance == 1:
+            return(3 * 10**-5)
+        else:
+            return(5 * 10**-7)
+            
        
-    def C2():
-        if compressor_off:
-            return(0 * 300**-1)
+    def C2(self, Temp):
+        if Temp > 5:
+            print(Temp)
+            #this turns the compressor on
+            return(8 * 10**-6)
         else:
-            return(8 * 10**-6 * 300**-1)
+            #this turn the compresso off
+            return(0 * 300** -1)
       
         
-    def Termostat(TM):
       
         
-for n in range(8640):
-    if n == 0:
-        PTime = 3.65
-    cooler(n, PTime)
-    doorChance= random.randrange(1,10)
-
-pass
+        
+        
 
 
+
+if __name__ == "__main__":
+    # Open csv file
+    with open('elpris.csv', newline='', encoding='utf-8') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        
+        # Reading the header
+        header = next(csvreader)  # Skipping to next header
+
+        # Reading and processing each row
+        for row in csvreader:
+            Tid = row['Tid']  # Time is in the first column
+            Pris = row['Pris']  # Price is in the second column
+            #print(f"Time: {Tid}, Price: {Pris}")
+
+    my_cooler = cooler()
+    for i in range(8640):
+        my_cooler.coolerupdate(i)
 
 
 
